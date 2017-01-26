ea6dbea1c8623a012cbd5bed86114cef3b87ea46
Patri129 <laditacyl@gmail.com>
2017-01-26 21:07:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <?php
+/*
+* In dit script worden errors in de vorm van JSON bijgehouden.
+*
+*/
+//-- Errors worden getoond -- 
 ini_set('display_errors', 1);
+//Errors bij startup worden getoond
 ini_set('display_startup_errors', 1);
+//Soort error, alle errors worden getoond
 error_reporting(E_ALL);
 
+//errors in een json file schrijven
 $json = fopen("stof.json","r");
 $string = fread($json,filesize("stof.json"));
+//de shit in een array zetten
 $stof = array();
 
 //convert json stream to an array
